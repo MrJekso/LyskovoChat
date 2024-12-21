ba8515b3948b18c4e32a17cd6faaddbbaad62fcb
Александр Антонов <56355757+MrJekso@users.noreply.github.com>
2024-12-21 16:55:10 -0500

    Add handler

--- backend/friends/app/main.py
+++ backend/friends/app/main.py
@@ -3,5 +3,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
+def home():
+    return {"message": "home"}
+
+@app.get("/test")
 def test():
-    return {"message": "OK"}
+    return {"message": "test"}
+
+@app.get("/frineds")
+def test():
+    return {"message": "frineds"}

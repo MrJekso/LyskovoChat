3a444b9bfdc44f1a4fefa96ca98a7bcd9f4f5df7
Антонов Александр <mr.jekso@gmail.com>
2025-01-12 22:44:58 +0000

    Append new handlers url

--- backend/friends/app/main.py
+++ backend/friends/app/main.py
@@ -2,10 +2,24 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "home"}
+################################
+@app.get("/healthcheck")
+def healthcheck():
+    return {"status": "ok"}
+################################
+@app.get('/profile')
+def profile():
+    return {"status":"ok","page":"profile"}
+@app.get('/friends')
+def friends():
+    return {"status":"ok","page":"friends"}
+@app.get('/news')
+def news():
+    return {"status":"ok","page":"news"}
+@app.get('/settings')
+def settings():
+    return {"status":"ok","page":"settings"}
+@app.get('/message')
+def message():
+    return {"status":"ok","page":"message"}
 
-@app.get("/test")
-def test():
-    return {"message": "test"}
